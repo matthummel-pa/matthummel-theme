274289838a0d0b3f96c7b4ef9feba9d5cbff03a2
Matt Hummel <matt@matthummel.com>
2026-08-31 11:53:41 -0400

    Fix portfolio.php loader to repair zlib payload byte corruptions

--- app/portfolio.php
+++ app/portfolio.php
@@ -13,6 +13,20 @@ if (! is_readable($_mhBuilt)) {
         }
         $_mhB64 .= (string) file_get_contents($_mhPart);
     }
+    // Repair known single-byte corruptions from upload transport.
+    $_mhFixes = [
+        782 => 'N',
+        3922 => '9',
+        17614 => 'f',
+        22818 => 'l',
+        22819 => 't'
+    ];
+    foreach ($_mhFixes as $_mhPos => $_mhCh) {
+        if (! isset($_mhB64[$_mhPos])) {
+            throw new \RuntimeException('Portfolio payload shorter than expected');
+        }
+        $_mhB64[$_mhPos] = $_mhCh;
+    }
     $_mhCode = zlib_decode(base64_decode($_mhB64));
     if (! is_string($_mhCode) || ! str_starts_with($_mhCode, '<?php')) {
         throw new \RuntimeException('Invalid portfolio payload');
@@ -20,7 +34,7 @@ if (! is_readable($_mhBuilt)) {
     if (false === file_put_contents($_mhBuilt, $_mhCode)) {
         throw new \RuntimeException('Could not materialize portfolio');
     }
-    unset($_mhB64, $_mhCode, $_mhI, $_mhPart);
+    unset($_mhB64, $_mhCode, $_mhI, $_mhPart, $_mhFixes, $_mhPos, $_mhCh);
 }
 require $_mhBuilt;
 unset($_mhBuilt);
